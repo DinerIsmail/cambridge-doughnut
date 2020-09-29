0477bfa4c8c01ef1a953e1415061b5f79f0cc8d2
DinerIsmail <ismail.diner@gmail.com>
2020-09-29 11:45:56 +0100

    Hide Cambridge City Council logo from Partners (for now)

--- components/partners/Partners.jsx
+++ components/partners/Partners.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import DealLogo from '../../assets/images/deal.jpg';
 import CityCouncilLogo from '../../assets/images/city-council.png';
-import ArticleWrapper from '../article-wrapper';
+
 import styles from './Partners.module.scss';
 
 const Partners = () => {
@@ -10,7 +10,7 @@ const Partners = () => {
 			<h2>Our Partners</h2>
 			<div className={styles.root}>
 				<img alt="Doughnut Economics Action Lab" src={DealLogo} />
-				<img alt="Cambridge City Council" src={CityCouncilLogo} />
+				{/* <img alt="Cambridge City Council" src={CityCouncilLogo} /> */}
 			</div>
 		</>
 	);
