d425556277eb15cfe2471c52e8f6800f9a4ec50e
DinerIsmail <ismail.diner@gmail.com>
2020-10-08 15:43:13 +0100

    Fix join us page width issue

--- components/signup-form/SignupForm.jsx
+++ components/signup-form/SignupForm.jsx
@@ -80,26 +80,30 @@ const SignupForm = () => {
 
 	return (
 		<section className={styles.signupForm}>
-			<h2>Register Interest</h2>
-			<p>
-				Register your interest in one or more of the following options.
-			</p>
-			<p>
-				Personal data we collect under GDPR is name and email address,
-				and any information you enter in the open fields. Please do not
-				submit information on someone else’s behalf.
-			</p>
-			<p>
-				If you have any questions, please get in touch with us at &nbsp;
-				<a href="mailto:cambridgedoughnut+privacy@gmail.com">
-					cambridgedoughnut+privacy@gmail.com
-				</a>
-				.
-			</p>
-			<p>
-				Our privacy policy:{' '}
-				<a href="https://bit.ly/319oCNE">https://bit.ly/319oCNE</a>
-			</p>
+			<div className={styles.description}>
+				<h3>Register Interest</h3>
+				<p>
+					Register your interest in one or more of the following
+					options.
+				</p>
+				<p>
+					Personal data we collect under GDPR is name and email
+					address, and any information you enter in the open fields.
+					Please do not submit information on someone else’s behalf.
+				</p>
+				<p>
+					If you have any questions, please get in touch with us at
+					&nbsp;
+					<a href="mailto:cambridgedoughnut+privacy@gmail.com">
+						cambridgedoughnut+privacy@gmail.com
+					</a>
+					.
+				</p>
+				<p>
+					Our privacy policy:{' '}
+					<a href="https://bit.ly/319oCNE">https://bit.ly/319oCNE</a>
+				</p>
+			</div>
 			<div>
 				<label className={styles.label} htmlFor="emailInput">
 					Email
