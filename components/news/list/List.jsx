c0731b8c42d3be2384c0e393820ae155f8f9696a
DinerIsmail <ismail.diner@gmail.com>
2020-10-18 12:23:32 +0100

    Fix newsroom styling

--- components/news/list/List.jsx
+++ components/news/list/List.jsx
@@ -4,18 +4,16 @@ import PropTypes from 'prop-types';
 import Item from './item';
 import styles from './List.module.scss';
 
-const List = ({ posts }) => {
-	return (
-		<>
-			<h1 className={styles.title}>Newsroom</h1>
-			<div className={styles.list}>
-				{posts.map((post) => (
-					<Item key={post.id} post={post} />
-				))}
-			</div>
-		</>
-	);
-};
+const List = ({ posts }) => (
+	<>
+		<h1>Newsroom</h1>
+		<div className={styles.list}>
+			{posts.map((post) => (
+				<Item key={post.id} post={post} />
+			))}
+		</div>
+	</>
+);
 
 List.propTypes = {
 	posts: PropTypes.array.isRequired,
